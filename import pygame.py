b7d93442f65ef851399509f27e2254ece925871d
DingDongBingBong1 <1meileo@hdsb.ca>
2025-11-26 11:25:55 -0500

    Update import pygame.py

--- import pygame.py
+++ import pygame.py
@@ -1,25 +1,33 @@
 import pygame
 pygame.init()
-Width = 1280
-Height = 720
+Width = 900
+Height = 900
 screen = pygame.display.set_mode((Width, Height))
-redSurface = pygame.Surface((Width,10))
-redSurface.fill((255,0,0))
-redSurface.set_alpha(100)
-blueSurface = pygame.Surface((Width,5))
-blueSurface.fill((255, 184, 61))
-blueSurface.set_alpha(200)
-greenSurface = pygame.Surface((10,Height))
-greenSurface.fill((86, 232, 205))
-greenSurface.set_alpha(130)
-screen.fill((227, 163, 159))
+ASurface = pygame.Surface((Width,5))
+ASurface.fill((100,0,0))
+ASurface.set_alpha(100)
+BSurface = pygame.Surface((Width,5))
+BSurface.fill((100,100,0))
+BSurface.set_alpha(100)
+CSurface = pygame.Surface((Width,5))
+CSurface.fill((100, 100, 100))
+CSurface.set_alpha(200)
+DSurface = pygame.Surface((10,Height))
+DSurface.fill((200, 100, 100))
+DSurface.set_alpha(130)
+ESurface = pygame.Surface((10,Height))
+ESurface.fill((200, 200, 100))
+ESurface.set_alpha(130)
+screen.fill((255, 255, 255))
 running = True
 i = 0
 for i in range(Height):
-    screen.blit(redSurface,(0,i*60))
-    screen.blit(blueSurface,(0,(i*60)+30))
+    screen.blit(ASurface,(0,(i*20)+5))
+    screen.blit(CSurface,(0,(i*20)+15))
+    screen.blit(BSurface,(0,(i*20)+30))
 for x in range(Width):
-    screen.blit(greenSurface,(x*40,0))
+    screen.blit(DSurface,((x*20)+5,0))
+    screen.blit(ESurface,((x*20)+10,0))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
