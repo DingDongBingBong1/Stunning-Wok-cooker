27019fecbf39e384e14da0893e4dec31016c6a4f
DingDongBingBong1 <1meileo@hdsb.ca>
2025-12-01 11:04:01 -0500

    e

--- import pygame.py
+++ import pygame.py
@@ -43,7 +43,7 @@ screen = pygame.display.set_mode((720, 720))
 pygame.display.set_caption("Window 2")
 
 # Load sound
-sound = pygame.mixer.Sound("i'll-take-a-potato-chip...and-eat-it!-made-with-Voicemod.mp3")
+sound = pygame.mixer.Sound("Voicy_I'll take a potato chip...and EAT IT.mp3")
 sound.set_volume(1.0)
 
 sound.play()
@@ -51,16 +51,15 @@ font1 = pygame.font.Font("it´s Scary Now St.ttf", 30)
 font2 = pygame.font.Font("NoScary-regular.ttf", 50)
 font3 = pygame.font.Font("Scary Brusher.ttf", 50)
 font4 = pygame.font.Font("Scary Pumpkin.ttf", 30)
-font5 = pygame.font.Font("BloodyTerror-GOW9Z.ttf", 150)
+font5 = pygame.font.Font("BloodyTerror-GOW9Z.ttf", 100)
 font6 = pygame.font.Font("Youmurdererbb-pwoK.otf", 50)
 running = True
 screen.fill((0, 0, 0))
-screen.blit(font1.render("I'm Going", True, (255, 255, 0)), (1, 50))
-screen.blit(font2.render("To Take", True, (255, 255, 0)), (125, 50))
-screen.blit(font3.render("a Potato", True, (255, 255, 0)), (250, 50))
-screen.blit(font4.render("Chip And...", True, (255, 255, 0)), (420, 55))
-screen.blit(font5.render("EAT IT!!!", True, (255, 255, 0)), (40, 150))
-screen.blit(font6.render("(Goofy Ahh Chuckle)", True, (255, 255, 0)), (40, 100))
+screen.blit(font1.render("I'll Take", True, (255, 255, 0)), (1, 50))
+screen.blit(font3.render("a Potato", True, (255, 255, 0)), (140, 50))
+screen.blit(font4.render("Chip...", True, (255, 255, 0)), (305, 55))
+screen.blit(font5.render("AND EAT IT!!!", True, (240, 26, 26)), (15, 150))
+screen.blit(font6.render("(Goofy Ahh Chuckle)", True, (201, 80, 24)), (40, 100))
 
 pygame.display.update()
 
